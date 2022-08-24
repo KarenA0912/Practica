f220fc592893e240eec78da4d8052738f402d623
Karen Andrea Rodriguez Garcia <krodriguezgi095@unal.edu.mx>
2022-08-23 20:16:13 -0500

    se agrego practica

---
+++ PracticaSuma.py
@@ -0,0 +1,17 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	primernumero = int()
+	segundonumero = int()
+	total = int()
+	print("****SUMA****")
+	print("Ingrese el primer número:")
+	primernumero = int(input())
+	print("Ingrese el segundo número:")
+	segundonumero = int(input())
+	total = primernumero+segundonumero
+	print("El resultado de la suma es: ",total)
+
